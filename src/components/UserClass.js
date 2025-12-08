272161f0c62f8af0a273df78a15d364eb33fcdbc
swapnil20711 <swapnil.bhojwani@gmail.com>
2025-12-09 00:15:16 +0530

    feat: added class based component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,11 +4,12 @@ class UserClass extends React.Component {
     super(props);
   }
   render() {
+    const {name,location,contact} = this.props;
     return (
       <div className="user-card">
-        <h2>Name : {this.props.name}</h2>
-        <h3>Location : Bengaluru</h3>
-        <h4>Contact : @swapnilbhojwani__</h4>
+        <h2>Name : {name}</h2>
+        <h3>Location : {location}</h3>
+        <h4>Contact : {contact}</h4>
       </div>
     );
   }
